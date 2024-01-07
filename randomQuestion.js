5595361f38a0db1e7efb196fe903ed7871934575
Ugnius Uscilas <uhhohum@gmail.com>
2024-01-07 13:45:54 +0200

    Rezultatas procentais po visu klausimu

--- randomQuestion.js
+++ randomQuestion.js
@@ -1,21 +1,23 @@
 "use strict";
 import question1 from "./data.js";
 import { answervalue } from "./answervalue.js";
+
 function randomNumberArray() {
-  //Sugeneruaja random array nuo 0-19
-  let questionLenght = question1.length;
+  // Generate a random array from 0 to 19
+  let questionLength = question1.length;
   let questionNumbers = [];
   let randomNumberList = [];
-  for (let i = 0; i < questionLenght; i++) {
+  for (let i = 0; i < questionLength; i++) {
     questionNumbers.push(i);
   }
-  while (questionLenght--) {
-    let j = Math.floor(Math.random() * (questionLenght + 1));
+  while (questionLength--) {
+    let j = Math.floor(Math.random() * (questionLength + 1));
     randomNumberList.push(questionNumbers[j]);
     questionNumbers.splice(j, 1);
   }
   return randomNumberList;
 }
+
 function questionDisplay() {
   let randomNumberList = randomNumberArray();
   const questionDisplay = document.createElement("h2");
@@ -26,40 +28,63 @@ function questionDisplay() {
   function displayNextQuestion() {
     const answerButtons = document.querySelectorAll(".awnserDisplay");
     answerButtons.forEach((el) => el.remove());
-    let currentQuestion = randomNumberList[randomQuestion];
-    questionDisplay.innerText = question1[currentQuestion].question;
-    for (let i = 0; i < question1[currentQuestion].answers.length; i++) {
-      const answerDisplay = document.createElement("button");
-      answerDisplay.setAttribute("class", "awnserDisplay");
-      answerDisplay.innerText = question1[currentQuestion].answers[i].text;
-      document.body.append(answerDisplay);
-      answerDisplay.addEventListener("click", (event) => {
-        const questionAnswers =
-          JSON.parse(localStorage.getItem("questionAnswers")) || [];
-        let answer = {
-          question: question1[currentQuestion].question,
-          answer: question1[currentQuestion].answers[i].text,
-          isCorrect: question1[currentQuestion].answers[i].isCorrect,
-        };
-        questionAnswers.push(answer);
-        localStorage.setItem(
-          "questionAnswers",
-          JSON.stringify(questionAnswers)
-        );
 
-        // Add a delay of 2 seconds before showing the next question
-        setTimeout(() => {
-          randomQuestion++;
-          if (randomQuestion < randomNumberList.length) {
+    if (randomQuestion < randomNumberList.length) {
+      let currentQuestion = randomNumberList[randomQuestion];
+      questionDisplay.innerText = question1[currentQuestion].question;
+      for (let i = 0; i < question1[currentQuestion].answers.length; i++) {
+        const answerDisplay = document.createElement("button");
+        answerDisplay.setAttribute("class", "awnserDisplay");
+        answerDisplay.innerText = question1[currentQuestion].answers[i].text;
+        document.body.append(answerDisplay);
+        answerDisplay.addEventListener("click", (event) => {
+          const questionAnswers =
+            JSON.parse(localStorage.getItem("questionAnswers")) || [];
+          let answer = {
+            question: question1[currentQuestion].question,
+            answer: question1[currentQuestion].answers[i].text,
+            isCorrect: question1[currentQuestion].answers[i].isCorrect,
+          };
+          questionAnswers.push(answer);
+          localStorage.setItem(
+            "questionAnswers",
+            JSON.stringify(questionAnswers)
+          );
+
+          // Add a delay of 2 seconds before showing the next question
+          setTimeout(() => {
+            randomQuestion++;
             displayNextQuestion();
-          } else {
-            console.log("Visi klausimai atsakyti");
-          }
-        }, 2000);
-      });
+          }, 2000);
+        });
+      }
+      answervalue();
+    } else {
+      // All questions answered, display results
+      displayResults();
     }
-    answervalue();
   }
+
+  function displayResults() {
+    const questionAnswers =
+      JSON.parse(localStorage.getItem("questionAnswers")) || [];
+    const totalQuestions = question1.length;
+    const correctAnswers = questionAnswers.filter(
+      (answer) => answer.isCorrect
+    ).length;
+    const percentage = (correctAnswers / totalQuestions) * 100;
+
+    // Display the results
+    const resultDisplay = document.createElement("div");
+    resultDisplay.setAttribute("id", "resultDisplay");
+    resultDisplay.innerHTML = `<p>Teisingi atsakymai: ${correctAnswers}</p>
+                               <p>Viso klausimu: ${totalQuestions}</p>
+                               <p>Rezultatas procentais: ${percentage.toFixed(
+                                 2
+                               )}%</p>`;
+    document.body.appendChild(resultDisplay);
+  }
+
   displayNextQuestion();
 }
 
