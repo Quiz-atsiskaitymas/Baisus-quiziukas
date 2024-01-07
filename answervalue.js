40e7b4dfacbf33087fc3ee708d8a29ffed42f54d
mantassapa <mantassapa@gmail.com>
2024-01-07 20:13:56 +0200

    dasideda klausimu pabaiga

--- answervalue.js
+++ answervalue.js
@@ -64,21 +64,6 @@ function answervalue() {
   const correctAnswerArray = [];
   const wrongAnswerArray = [];
 
-  //ending after 10 questions
-    if(QuestionCounter===11){
-        const bodyEl = document.querySelector('body')
-        const allDiv = document.querySelectorAll('body>div')
-        allDiv.forEach((el)=>(el.remove()))
-        const allh2 = document.getElementById('questionDisplay')
-        allh2.remove()
-        const allButton = document.querySelectorAll('body>button')
-        allButton.forEach((el)=>(el.remove()))
-        const congrat = document.createElement('h1')
-        congrat.innerText="congratulation"
-        bodyEl.append(congrat)
-        
-    }
-    
   const buttons = document.querySelectorAll(".awnserDisplay");
 
   if (document.getElementById("QuestionCounterDiv") === null) {
@@ -122,7 +107,21 @@ function answervalue() {
     });
   });
     //po paskutinio klausimo
-  
+    // ending after questions
+    if(document.getElementById("QuestionCounterDiv").childElementCount===20){//<-------pakeisti kiek klausimu reikia atsakyti
+      alert('no')
+        const bodyEl = document.querySelector('body')
+        const allDiv = document.querySelectorAll('body>div')
+        allDiv.forEach((el)=>(el.remove()))
+        const allh2 = document.getElementById('questionDisplay')
+        allh2.remove()
+        const allButton = document.querySelectorAll('body>button')
+        allButton.forEach((el)=>(el.remove()))
+        const congrat = document.createElement('h1')
+        congrat.innerText="congratulation"
+        bodyEl.append(congrat)
+    }
+      
 
 }
 
